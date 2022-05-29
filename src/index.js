429dae1454182591dc446c435e44ca01def6a55e
dowon <dowon@dowonui-MacBookAir.local>
2022-05-29 21:45:20 +0900

    History 기능 추가

--- src/index.js
+++ src/index.js
@@ -29,64 +29,16 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-
-
-  // 생성자를 통해 state를 초기화 합니다.
-  // js에서는 하위클래스의 생성자를 정의할 떄 항상 super를 호출.
-  // 모든 React 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출.
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    // 이렇게 배열을 직접 할당해도 되지만 slice를 통해서 배열을 복제해서 사용하자.
-    // const squares = this.state.squares;
-    const squares = this.state.squares.slice();
-
-    // 승자가 있거나, 이미 선택되었다면...
-    if (calculateWinner(squares)) {
-      alert('게임이 종료되었습니다. 승자는 ' + calculateWinner(squares));
-      return;
-    } else if (squares[i]) {
-      alert('이미 선택되었습니다.');
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState(
-      {
-        squares: squares,
-        xIsNext: !this.state.xIsNext
-      }
-    )
-  }
-
   renderSquare(i) {
     return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
     />;
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -108,15 +60,93 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+  // 생성자를 통해 state를 초기화 합니다.
+  // js에서는 하위클래스의 생성자를 정의할 떄 항상 super를 호출.
+  // 모든 React 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출.
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true,
+      stepNumber: 0,
+    };
+  }
+
+  handleClick(i) {
+    // 이렇게 배열을 직접 할당해도 되지만 slice를 통해서 배열을 복제해서 사용하자.
+    // const squares = this.state.squares;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    // 승자가 있거나, 이미 선택되었다면...
+    if (calculateWinner(squares)) {
+      alert('게임이 종료되었습니다. 승자는 ' + calculateWinner(squares));
+      return;
+    } else if (squares[i]) {
+      alert('이미 선택되었습니다.');
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState(
+      {
+        history: history.concat([{
+          squares: squares
+        }]),
+        xIsNext: !this.state.xIsNext,
+        stepNumber: history.length,
+      }
+    )
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+
+      return (
+        <li key={step}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
+    let status;
+
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            xIsNext={this.state.xIsNext}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div className="status">{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
