e251a8cc31f75778e2b4c8bcee4f5c24dad3a191
dowon <dowon@dowonui-MacBookAir.local>
2022-05-31 21:58:17 +0900

    time_travel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Board from './components/Board';
 
 // class Square extends React.Component {
 //   // props는 전달 받는 값.
@@ -16,48 +17,6 @@ import './index.css';
 //     );
 //   }
 // }
-
-// 함수 컴포넌트는 render 함수만을 가집니다.
-function Square(props) {
-  return (
-    <button
-      className="square"
-      onClick={props.onClick}>
-      {props.value}
-    </button>
-  )
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
-    />;
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
 
 class Game extends React.Component {
 
@@ -121,7 +80,7 @@ class Game extends React.Component {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
 
       return (
-        <li key={step}>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
